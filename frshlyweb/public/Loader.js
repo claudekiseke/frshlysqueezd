3b796c8900fd69f7b8a8255fb5c6566986967649
ck-laude <hi@cklaude.com>
2023-01-09 13:15:26 +0000

    image loader

---
+++ frshlyweb/public/Loader.js
@@ -0,0 +1,3 @@
+export default function myImageLoader() {
+    return `https://images.ctfassets.net/xa1los3yndi8/2EmZy1NTEJy881Hvxb6Q7S/99e7086f8b707965935e2fc74dbae26e/Spinner-1s-108px__1_.svg?w=100&q=75`
+  }
