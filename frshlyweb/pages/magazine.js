ca41f44209cb09373ec917b454692ff00eedf613
claudekiseke <93127737+claudekiseke@users.noreply.github.com>
2023-01-23 02:31:32 +0000

    medium api

---
+++ frshlyweb/pages/magazine.js
@@ -0,0 +1,11 @@
+export default function Magazine() {
+    const { parse } = require('rss-to-json');
+// async await
+(async () => {
+
+    var rss = await parse('https://blog.ethereum.org/feed.xml');
+
+    console.log(JSON.stringify(rss, null, 3));
+
+})();
+}
