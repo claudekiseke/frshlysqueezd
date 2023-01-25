dc41699c072a968f9ef1343fc7fe3e3550c3506b
claudekiseke <93127737+claudekiseke@users.noreply.github.com>
2023-01-25 22:25:11 +0000

    Update index.js

--- frshlyweb/pages/index.js
+++ frshlyweb/pages/index.js
@@ -81,7 +81,7 @@ export default function Landing() {
             <Head>
                 <title>Frshly Squeezd</title>
                 <meta charSet="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" user-scalable="no" >
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" user-scalable="no" />
             </Head>
             <div className={`container ${styles.container}`}>
                 <header className={styles.header}>
