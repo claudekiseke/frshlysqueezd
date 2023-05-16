967f65e6faaf9bc0bbc1c1cdf73f5ba89e3dc73b
Claude Kiseke <cks@codehousegroup.com>
2023-05-16 15:50:53 +0100

    remove router

--- frshlyweb/pages/account/forgot-password.js
+++ frshlyweb/pages/account/forgot-password.js
@@ -1,6 +1,4 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { auth, onAuthStateChanged } from "../../firebase/clientApp";
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import ForgotPassword from '../../components/Forms/Auth/ForgotPassword';
@@ -26,14 +24,6 @@ export async function getStaticProps() {
 }
 
 export default function forgotPassword ({ logo, navigation }) {
-
-  const router = useRouter();
-
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      router.push('/account/my-account')
-    }
-  });
 
   return (
     <div>
