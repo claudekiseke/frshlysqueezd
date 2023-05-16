f887c09ac08df17cd70837d3996b617ddba44aa8
Claude Kiseke <cks@codehousegroup.com>
2023-05-16 11:56:36 +0100

    update register form

--- frshlyweb/components/Forms/Auth/PersonalDetails.js
+++ frshlyweb/components/Forms/Auth/PersonalDetails.js
@@ -4,32 +4,36 @@ const PersonalDetails = ({ formData, setFormData, styles }) => {
         <input
           className={styles.login__textBox}
           id="signup__fname"
+          required
           type="text"
-          placeholder="First name"
+          placeholder="First name *"
           value={formData.fname}
           onChange={(e) =>
             setFormData({ ...formData, fname: e.target.value })} />
         <input
           className={styles.login__textBox}
           id="signup__lname"
+          required
           type="text"
-          placeholder="Last name"
+          placeholder="Last name *"
           value={formData.lname}
           onChange={(e) =>
             setFormData({ ...formData, lname: e.target.value })} />
         <input
           className={styles.login__textBox}
           id="signup__email"
+          required
           type="email"
-          placeholder="Email address"
+          placeholder="Email address *"
           value={formData.email}
           onChange={(e) =>
             setFormData({ ...formData, email: e.target.value })} />
         <input
           className={styles.login__textBox}
           id="signup__password"
+          required
           type="password"
-          placeholder="Password"
+          placeholder="Password *"
           value={formData.password}
           onChange={(e) =>
             setFormData({ ...formData, password: e.target.value })} />
